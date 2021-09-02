522846771565742f9e0ac808d2f7565a78767b54
Berry2460 <48969780+Berry2460@users.noreply.github.com>
2021-09-02 13:41:59 -0500

    old

--- compile.bat
+++
@@ -1,7 +0,0 @@
-set debug=false
-
-if not exist release md release
-if "%debug%"=="false" set window=-mwindows
-gcc src\main.c src\step.c src\draw.c src\window.c -x c -o release\main.exe %window% -L lib -lglfw3 -lglfw3dll -lopengl32 -lglu32 -lgdi32
-release\main.exe
-pause
