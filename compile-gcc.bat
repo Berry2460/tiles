946857dd3fe084663791ed20e09b91b6630519b6
Berry2460 <48969780+Berry2460@users.noreply.github.com>
2023-10-15 22:33:11 -0500

    Delete compile-gcc.bat

--- compile-gcc.bat
+++
@@ -1,8 +0,0 @@
-set debug=true
-
-if not exist release md release
-if "%debug%"=="false" set window=-mwindows 
-gcc -Wall -pthread src\main.c src/network.c src\level.c src\step.c src\draw.c src\window.c src\ai.c src\missiles.c src\player.c -x c -O2 -o release\main-gcc.exe %window%-L lib\gcc64 -lglfw3 -lglfw3dll -lopengl32 -lglu32 -lgdi32 -lws2_32
-cd release
-main-gcc.exe
-pause
