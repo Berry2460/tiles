56b4819a4715b86cd2cd327442c76f32a1deca69
Berry2460 <48969780+Berry2460@users.noreply.github.com>
2023-10-17 13:25:07 -0500

    Add files via upload

--- compile-gcc64.bat
+++ compile-gcc64.bat
@@ -2,7 +2,7 @@ set debug=false
 
 if not exist release md release
 if "%debug%"=="false" set window=-mwindows 
-gcc -Wall -pthread src\main.c src/network.c src\level.c src\step.c src\draw.c src\window.c src\ai.c src\missiles.c src\player.c -x c -O2 -o release\main-gcc.exe %window%-L lib -lglfw3 -lglfw3dll -lopengl32 -lglu32 -lgdi32 -lws2_32
+gcc -Wall -pthread src\*.c -x c -O2 -o release\main-gcc.exe %window%-L lib -lglfw3 -lglfw3dll -lopengl32 -lglu32 -lgdi32 -lws2_32 -llibwinpthread-1
 cd release
 main-gcc.exe
 pause
